2a94414dbae4b1c52610890fcbf145e1782c650f
baurus2 <juli0.cesar@ymail.com>
2021-11-09 20:48:56 -0300

    Exercicio variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,75 @@
+// Exercícios de interpretação de código
+
+/*Exer 1: 10
+10 5*/
+
+// Exer 2: 10 10 10 
+
+// Exer 3:
+
+// let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+// let recebidoDiaria = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${recebidoDiaria/horasTrabalhadas} por hora`)
+
+
+//Exercícios de escrita de código
+
+// Exer 1:
+
+// let nome
+// let idade
+// console.log(typeof nome, idade) // Foi atribuido undefined por que não foi atribuido um valor
+
+
+// let nome = prompt ("Qual é o seu nome?")
+// let idade = prompt ("Qual é a sua idade?")
+// console.log(typeof nome)
+// console.log(typeof idade) // Veio como string pois tudo que volta do prompt vem como string
+
+// let nomeUser
+// let idadeUser
+// console.log("Olá", nome, "você tem", idade, "anos")
+
+
+
+//Exer 2:
+
+// let sorvete = prompt ("Você gosta de sorvete?")
+// let marijane = prompt ("Você gosta de Marijane")
+// let larica = prompt ("Você está com Larica?")
+// console.log( sorvete)
+// console.log( marijane)
+// console.log( larica)
+
+//Exer 3:
+
+// let a = 10
+// let b = 25
+// let c 
+// c = a
+// a = b 
+// b = c 
+
+// console.log("O novo valor de a é", a) // O novo valor de a é 25
+// console.log("O novo valor de b é", b) // O novo valor de b é 10
+
+
+//Desafios 
+let numero1 = prompt("Digite um n°")
+let numero2 = prompt("Digite mais um n°")
+numero1 = Number(numero1)
+numero2 = Number(numero2)
+let soma = numero1 + numero2
+alert(soma)
+
+
+
+
+
+
+    
+
+
+
+
+
