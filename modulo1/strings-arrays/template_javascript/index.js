c627e7f67cebd6b924c022093a1a8c65d94c023a
baurus2 <juli0.cesar@ymail.com>
2021-11-17 05:13:54 -0300

    Aula de String e Array

---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,62 @@
+//Exercícios de interpretação de código
+/* Questão 1 
+A- undefined já que não let array não teve nenhuma atribuição
+B- vai ser null já que o array foi atribuido como null
+C- 11 
+D- 3
+E- Vai ficar 3, 19 ,5 ,6 ...
+F- 9
+ Questão 2
+ SUBI NUM ÔNIBUS EM MIRROCOS 27
+ =================================
+ 
+ Exercícios de escrita de código 
+ Questão 1*/
+//  const nome = prompt ("Qual o seu nome?")
+//  const email = prompt ("Qual o seu email?")
+//  console.log (`O email ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!`)
+// 2-
+// const rangos = ['sopa','sushi','lanche','pizza','bife']
+//A:
+// console.log(rangos)
+//B:
+// console.log("Essas são as minhas comidas preferidas: ")
+// console.log (rangos[0])
+// console.log (rangos[1])
+// console.log (rangos[2])
+// console.log (rangos[3])
+// console.log (rangos[4])
+//C:
+// const comidaUsuario = prompt ("Qual sua comida favorita")
+// rangos[1] = comidaUsuario
+// console.log (rangos)
+
+/*3- 
+A:*/
+// let listaDeTarefas = []
+// //B:
+// const tarefa1 = prompt ("Me fale uma tarefa que deve realizar no dia:")
+// const tarefa2 = prompt ("Me fale uma segunda tarefa que deve realizar no dia:")
+// const tarefa3 = prompt ("Me fale uma terceira tarefa que deve realizar no dia:")
+// listaDeTarefas.push (tarefa1 , tarefa2, tarefa3)
+// //C:
+// console.log (listaDeTarefas)
+// //D: 
+// let indice = Number(prompt("Digite o índice de uma tarefa que você já realizou: 0, 1 ou 2"))
+// //E:
+// let remover = listaDeTarefas.splice(indice, 1);
+// //F:
+// console.log (listaDeTarefas)
+/*Desafios
+1- */
+// const frase = "Expecto Patrono. Então quando chegar a hora , o garoto deve morrer?"
+// arrayFrase = frase.split(" ")
+// console.log (arrayFrase) //Sei la o enunciado me bugou, tem que ser com ou sem espaço de separação? A vou colocar mais um console.log com join kKKKK
+// console.log (arrayFrase.join())
+
+//2-
+const frutenhas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+indexFrutenhas = frutenhas.indexOf ("Abacaxi")
+console.log(indexFrutenhas , frutenhas.length)
+//salvando
+
