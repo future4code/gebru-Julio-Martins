6f2f6f0193c655d97bd28597b171148803f816b7
baurus2 <juli0.cesar@ymail.com>
2021-11-30 23:05:41 -0300

    Exercicio de lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,76 @@
+/*----------------------Exercícios de interpretação de código
+1-
+A: Irá ser impresso 10
+
+2-
+A: Serão impressos todos os numeros maiores que 18. 19, 21, 23, 25, 27, 30
+B: Sim, exemplo disso é o codigo do enunciado. 
+
+3-
+A:
+*
+**
+***
+****
+
+----------------------Exercícios de escrita de código
+1- A: B: e C:*/
+// let bichinhos = Number(prompt("Quantos bichinhos de estimação você tem?"))
+// let quantidadeAtual = 0
+// if (bichinhos === 0) {
+//     console.log("Que pena, você pode adotar um pet!")
+// } else {
+//     let bichos = []
+//     for (let i = 0; i < bichinhos; i++) {
+//         bichos.push(prompt("Digite os nomes dos seus pets."))
+//     }
+//     console.log(bichos)
+// }
+
+/*2-
+A: */
+let array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+let valorMinimo = 0
+let valorMaximo = 1000
+// const questaoA = array => {
+//     for (let i = 0; i < array.length; i++) {
+//         console.log(array[i])
+//     }
+// }
+// questaoA(array)
+
+//B:
+// const questaoB = array => {
+//     for (let i = 0; i < array.length; i++) {
+//         console.log(array[i] / 10)
+//     }
+// }
+// questaoB(array)
+
+//C:
+// let arrayC = []
+// const questaoC = array => {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % 2 === 0) {
+//             arrayC.push(array[i])
+//         }
+//     }
+//     console.log(arrayC)
+// }
+// questaoC(array)
+
+//D:
+
+// const questaoD = array => {
+//     for (let i = 0; i < array.length; i++) {
+//     console.log (`O elemento do índex é: ${array[i]}`)}
+// }
+// questaoD (array)
+// let arrayD = []
+// const questaoD = (array) => {
+
+//     for (let i = 0; i < array.length; i++) {
+//         if ("O elemento do índex é:")
+//     console.log (`O elemento do índex é: ${array2[i]}`)}
+// }
+// questaoD (array)
