eaeb9184a7199267698231e321073179b2101080
baurus2 <juli0.cesar@ymail.com>
2021-12-06 09:53:42 -0300

    Desafio do blackjack feito

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -1,31 +1,68 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+//Bem vindes :D
+alert("Lula 2022 :D")
 
-let player = []
-let pc = []
-let carta1 = true
-let carta2 = false
-const game = blackjack => {
-   if (confirm("Fala newbie, bora jogar blackjack?" + "\n" + "Quer iniciar uma nova rodada?")) {
-      while (!cards) {
-         player.push(comprarCarta())
-         player.push(comprarCarta())
-         pc.push(comprarCarta())
-         pc.push(comprarCarta())
+//Inicio do jogo de satanazes 
+
+const desafio = () => {
+   while (confirm("Fala newbie bora jogar 21?" + "\n" + "Se for medrose só correr!!!")) {
+      let cartasUser
+      let cartasDoPc
+      let pontosUser
+      let pontosDoPc
+      let compraDeCarta
+      do {
+         cartasUser = []
+         cartasDoPc = []
+         pontosUser = 0
+         pontosDoPc = 0
+         for (let index = 0; index < 2; index++) {
+            compraDeCarta = comprarCarta()
+            cartasUser.push(compraDeCarta.texto)
+            pontosUser += compraDeCarta.valor
+         }
+         for (let index = 0; index < 2; index++) {
+            compraDeCarta = comprarCarta()
+            cartasDoPc.push(compraDeCarta.texto)
+            pontosDoPc += compraDeCarta.valor
+         }
+
+      } while (((cartasUser[0].valor === 11 && cartasUser[1].valor === 11) ||
+         (cartasDoPc[0].valor === 11 && cartasDoPc[1].valor === 11)))
+
+      while (pontosUser <= 19 && confirm(`Suas cartas são: ${cartasUser.join(" ")}. A carta revelada do computador é: ${cartasDoPc[0]} `
+         + `\n` + "Vai comprar mais ou vai correr?")) {
+         compraDeCarta = comprarCarta()
+         cartasUser.push(compraDeCarta.texto)
+         pontosUser += compraDeCarta.valor
       }
-   } if ((player[0].valor == 11 && [1].valor == 11) ||
-      (pc[0].valor === 11 && pc[1].valor == 11)) {
-   } else {
+      if (pontosUser > 21) {
+         alert(`Suas cartas são: ${cartasUser.join(" ")}. Pontuação:: ${pontosUser}`
+            + `\n` + `As cartas do computador são: ${cartasDoPc.join(" ")}. Pontuação: ${pontosDoPc}`
+            + "\n" + `Computador venceu seu ruim. O/`)
+      } else {
+         while (pontosUser > pontosDoPc) {
+            compraDeCarta = comprarCarta()
+            cartasDoPc.push(compraDeCarta.texto)
+            pontosDoPc += compraDeCarta.valor
+         }
+         if (pontosDoPc > 21 || pontosUser > pontosDoPc) {
+            alert(`Suas cartas são: ${cartasUser.join(" ")}. Pontuação:: ${pontosUser}` + "\n" +
+               `As cartas do computador são:${cartasDoPc.join(" ")}. Pontuação: ${pontosDoPc}` + "\n" +
+               "É deu sorte.. Ganhou seu newbie")
+         } else
+            if (pontosUser < pontosDoPc) {
+               alert(`Suas cartas são: ${cartasUser.join(" ")}. Pontuação:: ${pontosUser}` + "\n" +
+                  `As cartas do computador são:${cartasDoPc.join(" ")}. Pontuação: ${pontosDoPc}` + "\n" +
+                  "Computador venceu a birosca toda. o/")
+            }
+            else {
+               alert(`Suas cartas são: ${cartasUser.join(" ")}. Pontuação:: ${pontosUser}` + "\n" +
+                  `As cartas do computador são:${cartasDoPc.join(" ")}. Pontuação: ${pontosDoPc}` + "\n" +
+                  "Empatamos --' aff")
+            }
+      }
+
    }
 }
-game()
+console.log ("Fim de jogo!")
+desafio()
