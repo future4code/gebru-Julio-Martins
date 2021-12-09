c3fa81b30ab6f70c7d23a890bd29a16be763df36
baurus2 <juli0.cesar@ymail.com>
2021-12-09 19:10:33 -0300

    Exercício 15B DESAFIO

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -13,7 +13,7 @@ function retornaTamanhoArray(array) {
 function retornaArrayInvertido(array) {
     /* Metodo normal*/
     // return array.reverse()
-    
+
     /*Desafio */
 
     var arraySecundario = []
@@ -199,5 +199,20 @@ function retornaArrayOrdenadoAlfabeticamente(consultas) {
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
+    consultas.sort(function (a, b) {
+        const dataA = a.dataDaConsulta.split("/")
+        const dataB = b.dataDaConsulta.split("/")
+
+        const dataFinalA = new Date(+dataA[2], dataA[1] - 1, +dataA[0]);
+        const dataFinalB = new Date(+dataB[2], dataB[1] - 1, +dataB[0]);
+
+        if (dataFinalA < dataFinalB) {
+            return -1;
+        } else {
+            return true;
+        }
+    })
+
+    return consultas
 
 }
