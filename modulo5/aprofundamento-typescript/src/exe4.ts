4212ed9b36dfabc8e881b35b309b1a9ed7aaa46f
baurus2 <juli0.cesar@ymail.com>
2022-05-26 05:48:55 -0300

    aprofundamento-typescrit

---
+++ modulo5/aprofundamento-typescript/src/exe4.ts
@@ -0,0 +1,39 @@
+// a) Crie um arquivo chamado exercicio-4.ts , cole o código abaixo e use comentários para responder as questões.
+
+// type pokemon = {
+// 	name: string,
+//   types: string,
+// 	healthPoints: number
+// }
+
+// const pokemon1: pokemon = {
+//   name: "Charmander",
+//   types: "Fire",
+//   healthPoints: 28
+// }
+
+// const pokemon2: pokemon = {
+//   name: "Bulbasaur",
+//   types: "Grass/Poison",
+//   healthPoints: 31
+// }
+
+// const pokemon3: pokemon = {
+//   name: "Squirtle",
+//   types: "Water",
+//   healthPoints: 35
+// }
+
+// b) Como você faria, já com a extensão instalada,
+// para transpilar(converter) esse arquivo typescript em um arquivo javascript?
+//RESPOSTA:
+// tsc exe4.ts
+
+// c) E se este arquivo estivesse dentro de uma pasta chamada src.
+// O processo seria diferente? Se sim, descreva as diferenças.
+//RESPOSTA:
+// tsc src/exe4.ts
+
+// d) Existe alguma maneira de transpilar múltilplos arquivos de uma vez só? Caso conheça, explique como fazer.
+//RESPOSTA:
+// tsc exe1.ts exe2.ts exe3.ts exe4.ts exe5.ts
