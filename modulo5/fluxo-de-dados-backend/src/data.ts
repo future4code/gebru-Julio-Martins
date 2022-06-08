236bbbd994f9873997b455603f2bf474ceb8ff30
baurus2 <juli0.cesar@ymail.com>
2022-06-08 04:29:09 -0300

    Exercicio Fluxo de dados Backend concluído

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,40 @@
+// Exercício 2
+
+type Product = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export const dataProducts : Product[] = [
+    {
+        id: "1",
+        name: "Banana Kush",
+        price: 260
+    },
+    {
+        id: "2",
+        name: "Sour Diesel",
+        price: 250
+    },
+    {
+        id: "3",
+        name: "White Widow",
+        price: 200
+    },
+    {
+        id: "4",
+        name: "Super Lemon Haze",
+        price: 5.75
+    },
+    {
+        id: "5",
+        name: "Platinum OG",
+        price: 299.99
+    },
+    {
+        id: "6",
+        name: "Purple Haze",
+        price: 250
+    }
+]
