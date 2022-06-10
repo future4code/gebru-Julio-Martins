43b42b6ad70cb76f78dbd443478a575399b03bd5
baurus2 <juli0.cesar@ymail.com>
2022-06-10 03:53:12 -0300

    Exercicios apis-rest

---
+++ modulo5/apis-rest/src/data.ts
@@ -0,0 +1,56 @@
+enum Type {
+    ADMIN = "ADMIN",
+    NORMAL = "NORMAL"
+}
+export type User = {
+    id: number,
+    name: string,
+    email:string,
+    type: "ADMIN" | "NORMAL",
+    age: number
+}
+
+export let users: User[] = [
+    {
+        id: 1,
+        name: "Alice",
+        email: "alice@email.com",
+        type: "ADMIN",
+        age: 12
+    },
+    {
+        id: 2,
+        name: "Bob",
+        email: "bob@email.com",
+        type: "NORMAL",
+        age: 36
+    },
+    {
+        id: 3,
+        name: "Coragem",
+        email: "coragem@email.com",
+        type: "NORMAL",
+        age: 21
+    },
+    {
+        id: 4,
+        name: "Dory",
+        email: "dory@email.com",
+        type: "NORMAL",
+        age: 17
+    },
+    {
+        id: 5,
+        name: "Elsa",
+        email: "elsa@email.com",
+        type: "ADMIN",
+        age: 17
+    },
+    {
+        id: 6,
+        name: "Fred",
+        email: "fred@email.com",
+        type: "ADMIN",
+        age: 60
+    }
+]
